0c1fbcc58e99cd2902dc6e3fce7e5f5114ec77c9
Joseph Miller <josephkmiller@yahoo.com>
2017-10-02 15:33:25 -0700

    Scrapped Initiative Idea. Need to come up with new one

--- Dungeons and Dragons Combat Tracker/Dungeons and Dragons Combat Tracker/Combat Tracker.Designer.cs
+++ Dungeons and Dragons Combat Tracker/Dungeons and Dragons Combat Tracker/Combat Tracker.Designer.cs
@@ -29,133 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.backButton = new System.Windows.Forms.Button();
-            this.nextButton = new System.Windows.Forms.Button();
-            this.doneButton = new System.Windows.Forms.Button();
-            this.nameLabel = new System.Windows.Forms.Label();
-            this.initiativeTextBox = new System.Windows.Forms.TextBox();
-            this.bonusInitTextBox = new System.Windows.Forms.TextBox();
-            this.totalInitTextBox = new System.Windows.Forms.TextBox();
-            this.initiativeLabel = new System.Windows.Forms.Label();
-            this.bonusInitLabel = new System.Windows.Forms.Label();
-            this.totalInitLabel = new System.Windows.Forms.Label();
-            this.plusOrMinusLabel = new System.Windows.Forms.Label();
-            this.equalLabel = new System.Windows.Forms.Label();
             this.initiativeTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
-            // 
-            // backButton
-            // 
-            this.backButton.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.backButton.Location = new System.Drawing.Point(507, 188);
-            this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(75, 23);
-            this.backButton.TabIndex = 0;
-            this.backButton.Text = "Back";
-            this.backButton.UseVisualStyleBackColor = true;
-            this.backButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.backButton_MouseClick);
-            // 
-            // nextButton
-            // 
-            this.nextButton.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nextButton.Location = new System.Drawing.Point(588, 188);
-            this.nextButton.Name = "nextButton";
-            this.nextButton.Size = new System.Drawing.Size(75, 23);
-            this.nextButton.TabIndex = 1;
-            this.nextButton.Text = "Next";
-            this.nextButton.UseVisualStyleBackColor = true;
-            this.nextButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.nextButton_MouseClick);
-            // 
-            // doneButton
-            // 
-            this.doneButton.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.doneButton.Location = new System.Drawing.Point(700, 188);
-            this.doneButton.Name = "doneButton";
-            this.doneButton.Size = new System.Drawing.Size(75, 23);
-            this.doneButton.TabIndex = 2;
-            this.doneButton.Text = "Done";
-            this.doneButton.UseVisualStyleBackColor = true;
-            // 
-            // nameLabel
-            // 
-            this.nameLabel.AutoSize = true;
-            this.nameLabel.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameLabel.Location = new System.Drawing.Point(206, 194);
-            this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(38, 15);
-            this.nameLabel.TabIndex = 3;
-            this.nameLabel.Text = "Name";
-            // 
-            // initiativeTextBox
-            // 
-            this.initiativeTextBox.Location = new System.Drawing.Point(289, 192);
-            this.initiativeTextBox.Name = "initiativeTextBox";
-            this.initiativeTextBox.Size = new System.Drawing.Size(50, 20);
-            this.initiativeTextBox.TabIndex = 4;
-            // 
-            // bonusInitTextBox
-            // 
-            this.bonusInitTextBox.Location = new System.Drawing.Point(373, 191);
-            this.bonusInitTextBox.Name = "bonusInitTextBox";
-            this.bonusInitTextBox.Size = new System.Drawing.Size(39, 20);
-            this.bonusInitTextBox.TabIndex = 5;
-            // 
-            // totalInitTextBox
-            // 
-            this.totalInitTextBox.Location = new System.Drawing.Point(440, 191);
-            this.totalInitTextBox.Name = "totalInitTextBox";
-            this.totalInitTextBox.Size = new System.Drawing.Size(50, 20);
-            this.totalInitTextBox.TabIndex = 6;
-            // 
-            // initiativeLabel
-            // 
-            this.initiativeLabel.AutoSize = true;
-            this.initiativeLabel.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.initiativeLabel.Location = new System.Drawing.Point(300, 175);
-            this.initiativeLabel.Name = "initiativeLabel";
-            this.initiativeLabel.Size = new System.Drawing.Size(26, 15);
-            this.initiativeLabel.TabIndex = 7;
-            this.initiativeLabel.Text = "Init";
-            // 
-            // bonusInitLabel
-            // 
-            this.bonusInitLabel.AutoSize = true;
-            this.bonusInitLabel.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bonusInitLabel.Location = new System.Drawing.Point(370, 175);
-            this.bonusInitLabel.Name = "bonusInitLabel";
-            this.bonusInitLabel.Size = new System.Drawing.Size(58, 15);
-            this.bonusInitLabel.TabIndex = 8;
-            this.bonusInitLabel.Text = "bonusInit";
-            // 
-            // totalInitLabel
-            // 
-            this.totalInitLabel.AutoSize = true;
-            this.totalInitLabel.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.totalInitLabel.Location = new System.Drawing.Point(437, 175);
-            this.totalInitLabel.Name = "totalInitLabel";
-            this.totalInitLabel.Size = new System.Drawing.Size(56, 15);
-            this.totalInitLabel.TabIndex = 9;
-            this.totalInitLabel.Text = "Total Init";
-            // 
-            // plusOrMinusLabel
-            // 
-            this.plusOrMinusLabel.AutoSize = true;
-            this.plusOrMinusLabel.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.plusOrMinusLabel.Location = new System.Drawing.Point(345, 194);
-            this.plusOrMinusLabel.Name = "plusOrMinusLabel";
-            this.plusOrMinusLabel.Size = new System.Drawing.Size(22, 15);
-            this.plusOrMinusLabel.TabIndex = 10;
-            this.plusOrMinusLabel.Text = "+/-";
-            // 
-            // equalLabel
-            // 
-            this.equalLabel.AutoSize = true;
-            this.equalLabel.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.equalLabel.Location = new System.Drawing.Point(418, 193);
-            this.equalLabel.Name = "equalLabel";
-            this.equalLabel.Size = new System.Drawing.Size(14, 15);
-            this.equalLabel.TabIndex = 11;
-            this.equalLabel.Text = "=";
             // 
             // initiativeTimer
             // 
@@ -166,40 +41,14 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(952, 420);
-            this.Controls.Add(this.equalLabel);
-            this.Controls.Add(this.plusOrMinusLabel);
-            this.Controls.Add(this.totalInitLabel);
-            this.Controls.Add(this.bonusInitLabel);
-            this.Controls.Add(this.initiativeLabel);
-            this.Controls.Add(this.totalInitTextBox);
-            this.Controls.Add(this.bonusInitTextBox);
-            this.Controls.Add(this.initiativeTextBox);
-            this.Controls.Add(this.nameLabel);
-            this.Controls.Add(this.doneButton);
-            this.Controls.Add(this.nextButton);
-            this.Controls.Add(this.backButton);
             this.Name = "Combat_Tracker";
             this.Text = "Combat_Tracker";
             this.Load += new System.EventHandler(this.Combat_Tracker_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button backButton;
-        private System.Windows.Forms.Button nextButton;
-        private System.Windows.Forms.Button doneButton;
-        private System.Windows.Forms.Label nameLabel;
-        private System.Windows.Forms.TextBox initiativeTextBox;
-        private System.Windows.Forms.TextBox bonusInitTextBox;
-        private System.Windows.Forms.TextBox totalInitTextBox;
-        private System.Windows.Forms.Label initiativeLabel;
-        private System.Windows.Forms.Label bonusInitLabel;
-        private System.Windows.Forms.Label totalInitLabel;
-        private System.Windows.Forms.Label plusOrMinusLabel;
-        private System.Windows.Forms.Label equalLabel;
         private System.Windows.Forms.Timer initiativeTimer;
     }
 }
--- Dungeons and Dragons Combat Tracker/Dungeons and Dragons Combat Tracker/Combat Tracker.cs
+++ Dungeons and Dragons Combat Tracker/Dungeons and Dragons Combat Tracker/Combat Tracker.cs
@@ -18,9 +18,6 @@ namespace Dungeons_and_Dragons_Combat_Tracker
             InitializeComponent();
             characters = Characters;
             initiativeTimer.Enabled = true;
-            initiativeTextBox.Text = "0";
-            bonusInitTextBox.Text = "0";
-            totalInitTextBox.Text = "0";
         }
 
         private void Combat_Tracker_Load(object sender, EventArgs e)
@@ -30,53 +27,12 @@ namespace Dungeons_and_Dragons_Combat_Tracker
 
         private void initiativeTimer_Tick(object sender, EventArgs e)
         {
-            if (initiativeTextBox.Text == "")
-            {
-                initiativeTextBox.Text = "0";
-            }
-            else if (bonusInitTextBox.Text == "")
-            {
-                bonusInitTextBox.Text = "0";
-            }
-            else if (totalInitTextBox.Text == "")
-            {
-                totalInitTextBox.Text = "0";
-            }
-            else
-            {
-                totalInitTextBox.Text = (int.Parse(initiativeTextBox.Text) + int.Parse(bonusInitTextBox.Text)).ToString();
-            }
+
         }
 
 
-        //WIP
-        int count = 0;
-        private void nextButton_MouseClick(object sender, MouseEventArgs e)
-        {
-            if (count < characters.Count)
-            {
-                nameLabel.Text = characters[count].Name;
-                characters[count].Initiative = int.Parse(initiativeTextBox.Text);
-                characters[count].BonusInitiative = int.Parse(bonusInitTextBox.Text);
-                characters[count].TotalInitiative = int.Parse(totalInitTextBox.Text);
-                initiativeTextBox.Clear();
-                bonusInitTextBox.Clear();
-                totalInitTextBox.Clear();
-                count++;
-            }
-        }
-
-        private void backButton_MouseClick(object sender, MouseEventArgs e)
-        {
-            count--;
-            if (count < 0)
-            {
-                count = 0;
-            }
-            nameLabel.Text = characters[count].Name;
-            initiativeTextBox.Text = characters[count].Initiative.ToString();
-            bonusInitTextBox.Text = characters[count].BonusInitiative.ToString();
-            totalInitTextBox.Text = characters[count].TotalInitiative.ToString();
-        }
+       
+     
+       
     }
 }
